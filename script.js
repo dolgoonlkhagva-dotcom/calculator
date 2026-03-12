733c5b5bd295fee302b3c7d1a54580604928ffb7
26bs86071 User <26bs86071@G302-CC0122.local>
2026-03-12 12:32:17 +0800

    update

---
+++ script.js
@@ -0,0 +1,17 @@
+const input = document.getElementById("input")
+const one = document.getElementById("one");
+const two = document.getElementById("two");
+const three = document.getElementById("three");
+const four = document.getElementById("four");
+const five = document.getElementById("five");
+const six = document.getElementById("six");
+const seven = document.getElementById("seven");
+const eight = document.getElementById("eight");
+const nine = document.getElementById("nine");
+const zero = document.getElementById("zero");
+// const one = document.getElementById("one");
+// const one = document.getElementById("one");
+// const one = document.getElementById("one");
+one.addEventListener("click", () => {
+    input.value += "1";
+});
